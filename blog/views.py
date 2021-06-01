4e9cfa8eb5cabaf69ceb79f7e97155ed1644c202
Jan Ruth <janr.coding@gmail.com>
2021-06-01 16:33:12 +0200

    added session 2

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author': 'JanR',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'May 05, 2021'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'May 06, 2021'
+    },
+]
+
+
 """
 Create functions that handle what page the user is supposed to see when using any specific link.
 For now we just return a simple httpresponse.
@@ -8,8 +24,11 @@ For now we just return a simple httpresponse.
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
